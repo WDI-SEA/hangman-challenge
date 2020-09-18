a5e1b9596b3fae79054cc6174806c259129dadf4
Elaine <elaine@Felipes-iMac.local>
2020-09-18 15:42:14 -0500

    add underscores for letter and input function, but may need to change underscores to be reliant on a different value depending on input returns

--- hangman.py
+++ hangman.py
@@ -6,3 +6,6 @@ words = ['dogs', 'couch', 'teapot', 'recursion', 'javascript']
 seed()
 secret_word = words[randint(0,(len(words)))]
 print(secret_word)
+# display the random word as underscores of the same length
+display_word = print('_'*len(secret_word))
+input('What\'s your guess?')
