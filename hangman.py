bbf1c4c9cc00cf29f6c821c4e48ac6802347fe0d
blangwell <b.langwell@outlook.com>
2020-09-18 12:21:05 -0700

    [hangman] Added hangman visual representation

--- hangman.py
+++ hangman.py
@@ -9,9 +9,27 @@ print(randint(0, len(word_bank)))
 
 class Game:
   def __init__(self):
-    self.word = word_bank[randint(0, len(word_bank))]
+    # get a word from a random index of word_bank
+    self.word = word_bank[randint(0, len(word_bank) - 1)]
+    self.game_over = True
+    self.hangman = '''
+    ___
+    |  o
+    | -|-
+    |  /\\
+    |
+    ______   
+    '''
 
   def __str__(self):
-    return self.word
+    # print current Game's word
+    return self.hangman
+
+class Word(Game):
+  def __init__(self):
+    # we pass in letters because the parent class
+    # needs access to that information
+    super().__init__()
+    self.letters = []
 
 print(Game())
