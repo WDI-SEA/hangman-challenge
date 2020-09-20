a313d8e824e5c96aa0c0e679cec48b20d981eb74
blangwell <b.langwell@outlook.com>
2020-09-20 12:18:51 -0700

    [hangman] Add take_turn method

--- hangman.py
+++ hangman.py
@@ -5,7 +5,8 @@ word_bank = ['booger', 'ostrich', 'dinosaur', 'tweaker', 'hamburger', 'orange', 
 
 # seed random number each time the script is run based on system time
 seed()
-print(randint(0, len(word_bank)))
+# DONE display secret word as underscores
+# 
 
 class Game:
   def __init__(self):
@@ -14,14 +15,22 @@ class Game:
     self.game_over = True
     self.hangman = '-o-|-<'
     self.letters = [] # display letters chosen by the player
-    self.display = '_ ' * len(self.word)
+    self.display = ['_'] * len(self.word)# generate a string/array the same len of self.word consisting only of underscores
 
   def __str__(self):
     # print current Game's word
-    return (
-      self.word + ' ' +
-      self.display
-    )
+    return f'''The secret word is {self.word} 
+    its length is {len(self.word)}, 
+    the game is displaying {self.display}, 
+    its len is {len(self.display)}'''
+
+  def take_turn(self):
+    turn = input('enter a letter\n')
+    print(f'HERES THE TURN {turn}')
+    if turn in self.word:
+      print('thats true')
+    else:
+      print('thats false')
 
 class Word(Game):
   def __init__(self):
@@ -31,8 +40,7 @@ class Word(Game):
     self.letters = []
 
 
-# TODO display secret word as underscores
-# generate a string/array the same len of self.word consisting only of underscores
 
 
-print(Game())
+new_game = print(Game())
+Game().take_turn()
