b1933c82e9c528681ea5e79d7c2e060fe5f419a3
Irene <airine@Irenes-MacBook-Air.local>
2020-09-20 15:58:27 -0700

    [hangman.py]set the variables

--- hangman.py
+++ hangman.py
@@ -11,4 +11,23 @@
 # Loop back up to step 6 and continue until the word is fully revealed or guesses are used up.
 
 import random
+from secret_words import secret_words
+import string
+
+def get_words(secret_words):
+    # randomly choose a secret_words from list
+    word = random.choice(secret_words)
+    # as long as the statement is true, it keeps iterating until it's not true
+    while '-' in word or ' ' in word:
+        word = random.choice(secret_words)
+    return word
+
+# Initialize the game: Initialize all variables to default values.
+def hangman():
+    # Randomly select a secret word.
+    word = get_words(secret_words)
+    word_letter = set(word)  # letter in word
+    alphabet = set(string.ascii_lowercase) # from english dictionary
+    used_letters = set() # Display the letters guessed so far.
+    numbers = 6 # Display hangman or number of guesses remaining.
 
