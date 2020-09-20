aa69ef93fcc5302df11d13d8ebc1c78f366396e8
alpha-martinez <martinezalphaa@gmail.com>
2020-09-20 17:44:59 -0400

    got something to render but it doesn't fully work

--- hangman.py
+++ hangman.py
@@ -1,34 +1,50 @@
-import random 
+import random
 
 words = ['apple', 'park', 'sleepy', 'hamburger', 'pizza', 'jellyfish']
 
-start = input('Welcome to hangman! You have 6 guesses. To start, press enter')
+start = input('Welcome to hangman! You have 6 attempts. To start, press enter')
+word = random.choice(words).lower()
 
-def hangman():
-    attempts = 6
-    word = random.choice(words).lower()
+
+def game_init():
+    attempts = 0
     # print(word)
-    guesses = 0
-    spaces = '-' * len(word)
+    guesses = 6
+    spaces = '- ' * len(word)
+    guessed_letters = []
+    reveal_word = ""
 
     print(spaces)
-    
-    while guesses < attempts:
+
+    while guesses > attempts:
+        print("Letters guessed:" + str(guessed_letters))
         user_answer = input("Choose a letter: ")
         for letters in word:
             if user_answer in word:
-                print("Letter " + user_answer + " was found!")
-                print(spaces) # need the letter to print here
+                print(f'Letter {user_answer} was found!')
+                guessed_letters.append(user_answer)
+                reveal_word = reveal_word + user_answer
+                print(reveal_word)
+                # if reveal_word == word:
+                #     print ('Congrats! You won!')
+                # else:
+                #     print('Keep trying!')
                 break
             else:
-                print("Letter " + user_answer + " was not found! Try again.")
-                guesses += 1
+                print(f'Letter {user_answer} was not found! Try again.')
+                guessed_letters.append(user_answer)
+                guesses -= 1
+                print(f'You have {guesses} remaining')
+                reveal_word = "_" + reveal_word
                 break
-    else: 
+
+    else:
         play_again = input('Would you like to play again? (y/n)')
         if play_again == 'y':
-            hangman()
+            game_init()
         else:
             print('Okay! Better luck next time!')
 
-hangman()
+game_init()
+
+
