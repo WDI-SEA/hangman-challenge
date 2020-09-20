7bfb2d9c2b9525677a6a494b0a89441520654d1f
Alice Huh <aklee213@gmail.com>
2020-09-20 03:06:31 -0400

    [hangman] fix remove all matched letter from the list and fix input field not showing when game is over

--- hangman.py
+++ hangman.py
@@ -8,20 +8,31 @@ word = ['banana', 'tree', 'plate', 'mirror', 'chair']
 
 # print(shuffle(word))
 
-def correct_letter(letter):
-    input_letter = input('insert letter')
+def correct_letter(letter,length):
+    
+    length = len(letter)
     print(letter)
-    if len(letter) == 1:
+    
+    # print('look',length)
+    if length == 0 :
         print('Game over')
         return
-    if input_letter in letter:
-        print('correct')
-        letter.remove(input_letter)
-        print (letter)
-        return correct_letter(letter)
     else:
-        print('try again')
-        return correct_letter(letter)
+        input_letter = input('insert letter')    
+        if input_letter in letter:
+            print('correct')
+            # print(length)
+            # letter.remove(input_letter)
+            letter = [x for x in letter if x != input_letter]
+            length = len([x for x in letter if x != input_letter])
+            
+            print (letter)
+            print(length)
+            
+            return correct_letter(letter,length)
+        else:
+            print('try again')
+            return correct_letter(letter,length)
 
 
 
@@ -33,7 +44,15 @@ def display_word(ls):
     # print(letter)
     print (len(letter)*'_ ')
 
-    return correct_letter(letter)
+    return correct_letter(letter,len(letter))
 
 display_word(word)
+
+
+
+
+# |-\
+# |  0
+# | /|\
+# | /| \
 
