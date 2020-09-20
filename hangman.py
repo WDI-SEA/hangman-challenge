6eb62f29679a20c8f6c6ab874e12b44f1af843d9
alpha-martinez <martinezalphaa@gmail.com>
2020-09-20 15:48:43 -0400

    got a restart game functionality going?

--- hangman.py
+++ hangman.py
@@ -11,21 +11,24 @@ def hangman():
     guesses = 0
     spaces = '-' * len(word)
 
-    print()
     print(spaces)
-    print()
     
     while guesses < attempts:
         user_answer = input("Choose a letter: ")
         for letters in word:
             if user_answer in word:
                 print("Letter " + user_answer + " was found!")
+                print(spaces) # need the letter to print here
                 break
             else:
                 print("Letter " + user_answer + " was not found! Try again.")
                 guesses += 1
                 break
     else: 
-        print("Game over!")
+        play_again = input('Would you like to play again? (y/n)')
+        if play_again == 'y':
+            hangman()
+        else:
+            print('Okay! Better luck next time!')
 
 hangman()
