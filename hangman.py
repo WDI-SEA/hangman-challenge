b508d83355bb7919afbcb4ee7dfed6503e294886
alpha-martinez <martinezalphaa@gmail.com>
2020-09-20 14:34:02 -0400

    got something going

--- hangman.py
+++ hangman.py
@@ -2,17 +2,30 @@ import random
 
 words = ['apple', 'park', 'sleepy', 'hamburger', 'pizza', 'jellyfish']
 
-# define hangman function
+start = input('Welcome to hangman! You have 6 guesses. To start, press enter')
+
 def hangman():
     attempts = 6
     word = random.choice(words).lower()
     # print(word)
-    guess = [''] 
-    correct_guess = ['']
+    guesses = 0
     spaces = '-' * len(word)
 
+    print()
+    print(spaces)
+    print()
+    
+    while guesses < attempts:
+        user_answer = input("Choose a letter: ")
+        for letters in word:
+            if user_answer in word:
+                print("Letter " + user_answer + " was found!")
+                break
+            else:
+                print("Letter " + user_answer + " was not found! Try again.")
+                guesses += 1
+                break
+    else: 
+        print("Game over!")
 
-
-
-#call function
 hangman()
