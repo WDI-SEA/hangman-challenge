1f8f05e6688feec330927827948e97432a17c44e
Alice Huh <aklee213@gmail.com>
2020-09-20 01:33:35 -0400

    [hanagman] Add function to check the correct letter and to decide when to end the game

--- hangman.py
+++ hangman.py
@@ -8,6 +8,24 @@ word = ['banana', 'tree', 'plate', 'mirror', 'chair']
 
 # print(shuffle(word))
 
+def correct_letter(letter):
+    input_letter = input('insert letter')
+    print(letter)
+    if len(letter) == 1:
+        print('Game over')
+        return
+    if input_letter in letter:
+        print('correct')
+        letter.remove(input_letter)
+        print (letter)
+        return correct_letter(letter)
+    else:
+        print('try again')
+        return correct_letter(letter)
+
+
+
+
 def display_word(ls):
     random.shuffle(ls)
     # print(ls[0])
@@ -15,6 +33,7 @@ def display_word(ls):
     # print(letter)
     print (len(letter)*'_ ')
 
-    return letter
+    return correct_letter(letter)
 
 display_word(word)
+
