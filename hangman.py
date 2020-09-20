ad5b105e4a821c4a0dc111251d165731014b5af4
blangwell <b.langwell@outlook.com>
2020-09-20 12:07:42 -0700

    [hangman] Create self.letters array to store guesses, self.display to display underscares in place of secret word characters

--- hangman.py
+++ hangman.py
@@ -3,7 +3,7 @@ from random import randint
 
 word_bank = ['booger', 'ostrich', 'dinosaur', 'tweaker', 'hamburger', 'orange', 'television', 'fungus', 'tarantula', 'bulging']
 
-# seed random number
+# seed random number each time the script is run based on system time
 seed()
 print(randint(0, len(word_bank)))
 
@@ -12,18 +12,16 @@ class Game:
     # get a word from a random index of word_bank
     self.word = word_bank[randint(0, len(word_bank) - 1)]
     self.game_over = True
-    self.hangman = '''
-    ___
-    |  o
-    | -|-
-    |  /\\
-    |
-    ______   
-    '''
+    self.hangman = '-o-|-<'
+    self.letters = [] # display letters chosen by the player
+    self.display = '_ ' * len(self.word)
 
   def __str__(self):
     # print current Game's word
-    return self.hangman
+    return (
+      self.word + ' ' +
+      self.display
+    )
 
 class Word(Game):
   def __init__(self):
@@ -31,5 +29,10 @@ class Word(Game):
     # needs access to that information
     super().__init__()
     self.letters = []
+
+
+# TODO display secret word as underscores
+# generate a string/array the same len of self.word consisting only of underscores
+
 
 print(Game())
