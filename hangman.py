8bfbf4b867e1812877b2930d585952e9e7fc9591
Alice Huh <aklee213@gmail.com>
2020-09-20 21:37:38 -0400

    [worng_answer] hangman image added for the worng answer and start new game when user fails 7 times

--- hangman.py
+++ hangman.py
@@ -8,17 +8,37 @@ word = ['banana', 'tree', 'plate', 'mirror', 'chair']
 
 # print(shuffle(word))
 
-def correct_letter(letter,length):
+
+def wrong_answer(count):
+    if count == 1:
+        print( "|-\ \n|  0 \n|    \n|")
+    elif count == 2:
+        print( "|-\ \n|  0 \n|  | \n|")
+    elif count == 3:
+        print("|-\ \n|  0 \n| /| \n|")
+    elif count == 4:
+        print("|-\ \n|  0 \n| /|\ \n|")
+    elif count == 5:
+        print("|-\ \n|  0 \n| /|\ \n|  | ")
+    elif count == 6:
+        print("|-\ \n|  0 \n| /|\ \n| /| ")
+    elif count == 7:
+        print("|-\ \n|  0 \n| /|\ \n| /|\ \n Game Over, try new game")
+        return display_word(word)
+    
+
+
+def correct_letter(letter,length,count=0):
     
     length = len(letter)
-    print(letter)
-    
+    # print(letter)
+    # print(count)
     # print('look',length)
     if length == 0 :
         print('Game over')
         return
     else:
-        input_letter = input('insert letter')    
+        input_letter = input('insert letter \n')    
         if input_letter in letter:
             print('correct')
             # print(length)
@@ -26,27 +46,34 @@ def correct_letter(letter,length):
             letter = [x for x in letter if x != input_letter]
             length = len([x for x in letter if x != input_letter])
             
-            print (letter)
-            print(length)
+            # print (letter)
+            # print(length)
             
-            return correct_letter(letter,length)
+            return correct_letter(letter,length,count)
         else:
             print('try again')
-            return correct_letter(letter,length)
-
-
-
+            count=count+1
+            return (wrong_answer(count),correct_letter(letter,length,count))
 
 def display_word(ls):
     random.shuffle(ls)
     # print(ls[0])
     letter = [char for char in ls[0]]
     # print(letter)
-    print (len(letter)*'_ ')
+    print ("|-\ \n|    \n|    \n| \n", len(letter)*'_ ')
 
     return correct_letter(letter,len(letter))
 
 display_word(word)
+
+
+
+
+
+
+
+
+
 
 
 
