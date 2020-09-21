51b0079baa4b0bef903e372ed7b2ec9d96c2d079
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-09-20 19:41:36 -0700

    add functions to select a random word and have the player input a letter

--- hangman.py
+++ hangman.py
@@ -1,3 +1,31 @@
-list = ["underscores", "corgis", "ramen", "pseudocode", "variables", "reveal", "preferably"]
+from random import choice
+# list of words
+# choice method selects a random word from list 
+list = choice(["underscores", "corgis", "ramen", "pseudocode", "variables", "reveal", "preferably"])
+print(list)
 
-# choose a random word from list
+out = ""
+# for loop 
+for letter in list:
+    out = out + "_"
+print("Guess a letter:", out)
+
+# input find out what the player typed
+guess = input()
+
+#find out if the letter is in the word
+if guess in list:
+    print("Correct! 😁")
+else:
+    print("Try again")
+
+# game functionality
+# def hangman(guesses):
+#     # declare number of turns player will have
+#     turns = 7
+#     # randomly chooses a word from list
+#     random_word = random.choice(list).lower()
+#     correct_guess = [""]
+
+
+# def getRandomWord(words):
